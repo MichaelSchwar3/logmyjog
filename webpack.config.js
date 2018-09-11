82b1f96f6b1d73b7bacfd528d0d4164a910327b8
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-09-11 11:48:54 -0400

    Create package JSON and webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.join(__dirname, "frontend", "log_my_jog.jsx"),
+  output: {
+    path: path.join(__dirname, "app", "assets", "javascripts"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  },
+  devtool: "eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
