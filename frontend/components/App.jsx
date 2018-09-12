7241c71f2bc93000adb2d4692b9268218febae7d
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-09-12 16:50:44 -0400

    Complete Footer

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,19 +2,21 @@ import React from 'react';
 import GreetingContainer from './greeting/greeting_container';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
+import Footer from './footer/footer';
 import { AuthRoute, Protected } from '../util/route_util';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 
 const App = () => (
   <div>
     <header>
-      <h1><span>&#8984;</span>  L O G M Y J O G</h1>
+      <Link to="/"><h1><span>&#8984;</span>  L O G M Y J O G</h1></Link>
       <Route exact path='/' component={GreetingContainer} />
     </header>
 
     <AuthRoute exact path="/login" component={LoginFormContainer} />
     <AuthRoute exact path="/signup" component={SignupFormContainer} />
+    <Route exact path='/' component={Footer} />
   </div>
 );
 
---
+++ frontend/components/footer/footer.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+  return(
+    <div id="footer-bar">
+      <div id="iphone-img">
+        <img src={'/assets/iphone.png'}/>
+      </div>
+      <div id="footer-social">
+        <ul>
+          <li>SOCIAL</li>
+          <li><a href="#">LinkedIn</a></li>
+          <li><a href="#">Github</a></li>
+          <li><a href="#">Facebook</a></li>
+        </ul>
+      </div>
+      <div id="footer-help">
+        <ul>
+          <li>HELP</li>
+          <li><Link to='/login'>Login/Register</Link></li>
+          <li><a href="https://www.google.com">Google</a></li>
+          <li><a href="https://www.stackoverflow.com">StackOverFlow</a></li>
+        </ul>
+      </div>
+      <div id="footer-about">
+        <ul>
+          <li>ABOUT ME</li>
+          <li><a href="#">NY Giants</a></li>
+          <li><a href="#">Athlinks</a></li>
+          <li><a href="#">Movies</a></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Footer;
