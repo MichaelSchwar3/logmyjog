8d5316c1105a7ef7ecc0a0ab1fe7e1e84ec54223
Michael Schwartz <MichaelSchwar3@gmail.com>
2018-10-15 18:09:59 -0400

    Fix syntax in time util

--- frontend/util/date_util.js
+++ frontend/util/date_util.js
@@ -1,16 +1,16 @@
 export const formatDate = date => {
-  if(!date) return ""
-  const month = date.slice(5,7)
-  const day = date.slice(8,10)
-  const year = date.slice(0,4)
+  if(!date) return "";
+  const month = date.slice(5,7);
+  const day = date.slice(8,10);
+  const year = date.slice(0,4);
   return `${month}/${day}/${year}`;
 };
 
 export const formatTime = (duration) => {
-  const fullHours = (duration -(duration %3600))/3600
-  duration = duration - (fullHours * 3600)
-  const minutes = (duration - (duration % 60))/60
-  const seconds = duration - (minutes * 60)
+  const fullHours = (duration -(duration %3600))/3600;
+  duration = duration - (fullHours * 3600);
+  const minutes = (duration - (duration % 60))/60;
+  const seconds = duration - (minutes * 60);
   const tmpMin = `0${minutes}`;
   const tmpSec = `0${seconds}`;
   const paddedMinutes = tmpMin.slice(tmpMin.length - 2);
@@ -20,10 +20,10 @@ export const formatTime = (duration) => {
 
 export const formatDuration = (hours, mins, secs) => {
   secs += (hours * 3600) + (mins * 60);
-  return secs
-}
+  return secs;
+};
 
 export const formatPace = (seconds, distance) => {
   let pace = formatTime(Math.floor(seconds/distance));
   return pace.slice(3);
-}
+};
